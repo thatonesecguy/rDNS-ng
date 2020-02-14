30042d37556cfac4e03eea7d3ae33f2fbc8f007d
Harshil Shah <harshil.r1999@gmail.com>
2020-02-14 10:57:17 +0530

    Update rDNS.py

--- rDNS.py
+++ rDNS.py
@@ -16,6 +16,8 @@ def banner():
         print(f'{Fore.GREEN}%s{Style.RESET_ALL}' % data)
         print("Providing you with Reverse DNS on subnets, IP ranges and IP lists")
         print("Author : Harshil Shah (@harshil-shah004)")
+	print("\n")
+	print("\n")
 
 banner()
 
